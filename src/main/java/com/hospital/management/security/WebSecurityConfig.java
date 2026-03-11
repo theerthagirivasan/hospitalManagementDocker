051fd6488a3e00fae9c4975647714573b4a3086a
theerthagirivasan <theerthagirivasan@gmail.com>
2026-03-11 22:14:09 +0530

    config updated

--- src/main/java/com/hospital/management/security/WebSecurityConfig.java
+++ src/main/java/com/hospital/management/security/WebSecurityConfig.java
@@ -80,6 +80,7 @@ public class WebSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*")); // Update in production
+        configuration.addAllowedOrigin("https://hospitalmanagementdocker.vercel.app");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
